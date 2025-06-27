64fe22fd4159e3188d9283493437dfa4a23efcb4
Zhanna <kezhanna.03@mail.ru>
2025-06-27 11:58:20 +0500

    Headers in Route Handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,14 @@
-export async function GET(){
-    return new Response('Profile API Data');
+import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(request: NextRequest) {
+  // const requestHeaders = new Headers(request.headers);
+  // console.log(requestHeaders.get("Authorization"))
+
+  const headerList = await headers();
+  console.log(headerList.get("Authorization"));
+
+  return new Response("<h1>Profile API Data</h1>", {
+    headers: { "Content-type": "text/html" },
+  });
 }
