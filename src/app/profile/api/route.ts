c82e210d46e970a3c0300e4f0a01e110f2c48d5f
Zhanna <kezhanna.03@mail.ru>
2025-06-27 12:18:38 +0500

    Cookies in Route Handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,5 +1,5 @@
 import { type NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
   // const requestHeaders = new Headers(request.headers);
@@ -8,7 +8,15 @@ export async function GET(request: NextRequest) {
   const headerList = await headers();
   console.log(headerList.get("Authorization"));
 
+  const theme = request.cookies.get("theme");
+  console.log(theme)
+
+  const cookiesStore = await cookies();
+  cookiesStore.set("resultsPerPage", "20");
+
   return new Response("<h1>Profile API Data</h1>", {
-    headers: { "Content-type": "text/html" },
+    headers: { "Content-type": "text/html" ,
+        "Set-Cookie": "theme=dark",
+    },
   });
 }
