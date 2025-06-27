546ada1a05b31d2d3c01e7249911d5b7c5b3e5a5
Zhanna <kezhanna.03@mail.ru>
2025-06-27 11:13:35 +0500

    Handling DELETE Request

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -16,8 +16,18 @@ export async function PATCH(
   const { id } = await params;
   const body = await request.json();
   const { text } = body;
-  
+
   const index = comments.findIndex((comment) => comment.id === parseInt(id));
   comments[index].text = text;
   return Response.json(comments[index]);
+}
+
+export async function DELETE(_request:Request,
+    {params}:{params:Promise<{id:string}>}
+){
+    const {id} = await params;
+    const index = comments.findIndex((comment)=> comment.id === parseInt(id));
+    const deletedComment = comments[index];
+    comments.splice(index, 1);
+    return Response.json(deletedComment);
 }
